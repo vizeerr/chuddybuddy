23e0897123d4e7453f57bc39765519eda6c39758
vivek sagar <vivek54540@gmail.com>
2025-03-05 16:43:29 +0530

    first commit v1.5

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -22,12 +22,12 @@ import type { User, Expense } from "@/lib/types"
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyBQpv-U7zjVgPTbfK7RMHg05eWkICPg4aA",
+  authDomain: "monkhood-55245.firebaseapp.com",
+  projectId: "monkhood-55245",
+  storageBucket: "monkhood-55245.appspot.com",
+  messagingSenderId: "567445925428",
+  appId: "1:567445925428:web:afde7efc78f65b381ca461"
 }
 
 // Initialize Firebase
